fbb5fc9a3fc0485b4104edd2c1e2bf6e70025034
Toni <81023569+TorcaST@users.noreply.github.com>
2021-08-09 14:21:33 +0200

    upload docker image to dockerhub

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=myapp:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag $dockerpath tonisi4568489/testmyapp:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push tonisi4568489/testmyapp:latest
