a21f2bf7b1ee5bb00ed9aaa15634751839732d98
Toni <81023569+TorcaST@users.noreply.github.com>
2021-08-09 14:21:58 +0200

    run kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="tonisi4568489/testmyapp:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run myappdemo --image="tonisi4568489/testmyapp:latest" --port=80 --labels app=myappdemo
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+sleep 30s
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward myappdemo 80:80
